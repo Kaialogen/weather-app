ba8eda161e6fdca9c714faf0cd3a61f3026f3c70
Kaialogen <kaialogen@protonmail.com>
2025-09-15 19:28:58 +0100

    feat: Update dependencies and enhance Searchbar component with utility functions

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,12 +1,26 @@
+import { cn } from "@/utils/cn";
 import { IoSearch } from "react-icons/io5";
 
-type Props = {};
+type Props = {
+  className?: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
+  onSubmit: React.FormEventHandler<HTMLFormElement> | undefined;
+};
 
-export default function Searchbar({}: Props) {
+export default function Searchbar(props: Props) {
   return (
-    <form className="flex relative items-center justify-center h-10">
+    <form
+      onSubmit={props.onSubmit}
+      className={cn(
+        "flex relative items-center justify-center h-10",
+        props.className
+      )}
+    >
       <input
         type="text"
+        value={props.value}
+        onChange={props.onChange}
         placeholder="Location"
         className="px-4 py-2 w-[230px] border border-gray-300 rounded-l-md focus:outline-none focus:border-blue-500 h-full"
       />
