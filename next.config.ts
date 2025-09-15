56c697ef37e8fb96309d06f4b73c56e0b5c033dc
Kaialogen <kaialogen@protonmail.com>
2025-09-15 23:26:35 +0100

    feat: Implemented weather icons

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+      },
+    ],
+  },
+};
+
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import axios from "axios";
 import { format, parseISO } from "date-fns";
 import Container from "@/components/Container";
 import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
+import WeatherIcon from "@/components/WeatherIcon";
 
 interface WeatherData {
   cod: string;
@@ -86,55 +87,58 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
-      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4"></main>
-      <section className="space-y-4">
-        <div className="space-y-2">
-          <h2 className="flex gap-1 text-2xl items-end">
-            <p> {format(parseISO(firstData?.dt_txt ?? ""), "EEEE")} </p>
-            <p className="text-lg">
-              {" "}
-              ({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")}){" "}
-            </p>
-          </h2>
-          <Container className="gap-10 px-6 items-center">
-            <div className="flex flex-col px-4">
-              <span className="text-5xl">
-                {convertKelvinToCelsius(firstData?.main.temp ?? 0)}°C
-              </span>
-              <p className="text-xs space-x-1 whitespace-nowrap">
-                <span>Feels like</span>
-                <span>
-                  {convertKelvinToCelsius(firstData?.main.feels_like ?? 0)}°C
-                </span>
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        <section className="space-y-4">
+          <div className="space-y-2">
+            <h2 className="flex gap-1 text-2xl items-end">
+              <p> {format(parseISO(firstData?.dt_txt ?? ""), "EEEE")} </p>
+              <p className="text-lg">
+                {" "}
+                ({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")}){" "}
               </p>
-              <p className="text-xs space-x-2">
-                <span>
-                  Min: {convertKelvinToCelsius(firstData?.main.temp_min ?? 0)}°C{" "}
+            </h2>
+            <Container className="gap-10 px-6 items-center">
+              <div className="flex flex-col px-4">
+                <span className="text-5xl">
+                  {convertKelvinToCelsius(firstData?.main.temp ?? 0)}°C
                 </span>
-                <span>
-                  {" "}
-                  Max: {convertKelvinToCelsius(firstData?.main.temp_max ?? 0)}°C
-                </span>
-              </p>
-            </div>
-            <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
-              {data?.list.map((data, index) => (
-                <div
-                  key={index}
-                  className="flex flex-col justify-between gap-2 items-center text-xs font-semibold"
-                >
-                  <p className="whitespace-nowrap">
-                    {format(parseISO(data.dt_txt), "h:mm a")}
-                  </p>
-
-                  <p>{convertKelvinToCelsius(data?.main.temp ?? 0)}°C</p>
-                </div>
-              ))}
-            </div>
-          </Container>
-        </div>
-      </section>
-      <section></section>
+                <p className="text-xs space-x-1 whitespace-nowrap">
+                  <span>Feels like</span>
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.feels_like ?? 0)}°C
+                  </span>
+                </p>
+                <p className="text-xs space-x-2">
+                  <span>
+                    Min: {convertKelvinToCelsius(firstData?.main.temp_min ?? 0)}
+                    °C{" "}
+                  </span>
+                  <span>
+                    {" "}
+                    Max: {convertKelvinToCelsius(firstData?.main.temp_max ?? 0)}
+                    °C
+                  </span>
+                </p>
+              </div>
+              <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
+                {data?.list.map((data, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-col justify-between gap-2 items-center text-xs font-semibold"
+                  >
+                    <p className="whitespace-nowrap">
+                      {format(parseISO(data.dt_txt), "h:mm a")}
+                    </p>
+                    <WeatherIcon iconName={data.weather[0].icon} />
+                    <p>{convertKelvinToCelsius(data?.main.temp ?? 0)}°C</p>
+                  </div>
+                ))}
+              </div>
+            </Container>
+          </div>
+        </section>
+        <section></section>
+      </main>
     </div>
   );
 }
