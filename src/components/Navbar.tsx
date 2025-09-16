b8edc884f7703b85333ce6955af2d59d1f961387
Kaialogen <kaialogen@protonmail.com>
2025-09-16 18:19:10 +0100

    feat: Added geolocation functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -62,6 +62,27 @@ export default function Navbar({ location }: Props) {
     }
   };
 
+  const handleCurrentLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const { latitude, longitude } = position.coords;
+        try {
+          setLoadingCity(true);
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+          );
+          setTimeout(() => {
+            setLoadingCity(false);
+            setPlace(response.data.name);
+          }, 500);
+        } catch (error) {
+          console.log(error);
+          setLoadingCity(false);
+        }
+      });
+    }
+  };
+
   return (
     <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
       <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
@@ -70,7 +91,11 @@ export default function Navbar({ location }: Props) {
           <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
         </p>
         <section className="flex gap-2 items-center">
-          <MdMyLocation className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer" />
+          <MdMyLocation
+            title="Your Current Location"
+            onClick={handleCurrentLocation}
+            className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer"
+          />
           <MdOutlineLocationOn className="text-3xl" />
           <p className="text-slate-900/80 text-sm"> {location} </p>
           <div className="relative">
